3235e886a9bd53642469121b1bffd1febe0ad916
lyrical logical <lyrical.logical@gmail.com>
2012-03-20 18:35:32 +0900

    use Typed Resources

--- src_scala/jp/lyrilogi/sbt/AndroidSbtScalaActivity.scala
+++ src_scala/jp/lyrilogi/sbt/AndroidSbtScalaActivity.scala
@@ -3,12 +3,14 @@ package jp.lyrilogi.sbt
 import android.app.Activity
 import android.os.Bundle
 
-class AndroidSbtScalaActivity extends Activity
+class AndroidSbtScalaActivity extends Activity with TypedViewHolder
 {
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.scala_main);
+
+    findView(TR.textView).setText("hello sbt android-plugin");
   }
 }
 
